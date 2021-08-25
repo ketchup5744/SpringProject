7ed362814c8a67aa301f68e4478a0def14e8a672
Josh <vkd5744@naver.com>
2021-08-25 17:33:54 +0900

    게시판 목록 구현
    DB의 주키 내림차순으로 정렬

--- src/main/java/com/rilla/dao/BoardDAO.java
+++ src/main/java/com/rilla/dao/BoardDAO.java
@@ -1,9 +1,14 @@
 package com.rilla.dao;
 
+import java.util.List;
+
 import com.rilla.vo.BoardVO;
 
 public interface BoardDAO {
 
 	// 게시글 작성
 	public void write(BoardVO boardVO) throws Exception;
+	
+	// 게시물 목록 조회
+		public List<BoardVO> list() throws Exception;
 }
--- src/main/java/com/rilla/service/BoardService.java
+++ src/main/java/com/rilla/service/BoardService.java
@@ -1,10 +1,15 @@
 package com.rilla.service;
 
+import java.util.List;
+
 import com.rilla.vo.BoardVO;
 
 public interface BoardService {
 
 	// 게시글 작성
 	public void write(BoardVO boardVO) throws Exception;
+	
+	// 게시물 목록 조회
+		public List<BoardVO> list() throws Exception;
 	
 }
